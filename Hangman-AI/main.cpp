e871b0d0295f875a6dcc4bce2492b68a51206ddf
Asoby <itsasoby@gmail.com>
2020-06-20 17:31:12 +0200

    Initial Commit

---
+++ Hangman-AI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hangman-AI
+//
+//  Created by Asoby on 6/20/20.
+//  Copyright © 2020 Asoby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
